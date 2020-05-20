ec50f9fe76a8589d500509b70d7b0de56a22b145
Boris <new.boris@mail.ru>
2020-05-20 22:07:32 +0300

    Homework for lesson5

--- mainapp/views.py
+++ mainapp/views.py
@@ -2,6 +2,8 @@ from django.conf import settings
 from django.shortcuts import render
 from datetime import datetime
 from .models import Product, ProductCategory, Location
+from django.shortcuts import get_object_or_404
+from basketapp.models import Basket
 
 
 def main(request):
@@ -14,12 +16,36 @@ def main(request):
 def products(request, pk=None):
     title = "продукты"
     links_menu = ProductCategory.objects.all()
+
+    basket = []
+    if request.user.is_authenticated:
+        basket = Basket.objects.filter(user=request.user)
+
+    if pk is not None:
+        if pk == 0:
+            products = Product.objects.all().order_by("price")
+            category = {"name": "все"}
+        else:
+            category = get_object_or_404(ProductCategory, pk=pk)
+            products = Product.objects.filter(category__pk=pk).order_by("price")
+
+        content = {
+            "title": title,
+            "links_menu": links_menu,
+            "category": category,
+            "products": products,
+            "media_url": settings.MEDIA_URL,
+            "basket": basket,
+        }
+        return render(request, "mainapp/products_list.html", content)
+
     same_products = Product.objects.all()
     content = {
         "title": title,
         "links_menu": links_menu,
         "same_products": same_products,
         "media_url": settings.MEDIA_URL,
+        "basket": basket,
     }
     return render(request, "mainapp/products.html", content)
 
