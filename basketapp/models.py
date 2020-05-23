5fc8edd73c81734c69f0dacdb955727f419b829a
boris <new.boris@mail.ru>
2020-05-23 23:28:42 +0300

    Homework for lesson6

--- basketapp/models.py
+++ basketapp/models.py
@@ -9,3 +9,23 @@ class Basket(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(verbose_name='количество', default=0)
     add_datetime = models.DateTimeField(verbose_name='время', auto_now_add=True)
+
+    @property
+    def product_cost(self):
+        """return cost of all products this type"""
+        return self.product.price * self.quantity
+
+    @property
+    def total_quantity(self):
+        """return total quantity for user"""
+        items = Basket.objects.filter(user=self.user)
+        totalquantity = sum([x.quantity for x in items])
+        return totalquantity
+
+    @property
+    def total_cost(self):
+        """return total cost for user"""
+        items = Basket.objects.filter(user=self.user)
+        totalcost = sum([x.product_cost for x in items])
+        return totalcost
+
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,4 @@
+import random
 from django.conf import settings
 from django.shortcuts import render
 from datetime import datetime
@@ -6,20 +7,35 @@ from django.shortcuts import get_object_or_404
 from basketapp.models import Basket
 
 
+def get_basket(user):
+    return Basket.objects.filter(user=user) if user.is_authenticated else []
+
+
+def get_hot_product():
+    products = Product.objects.all()
+    return random.sample(list(products), 1)[0]
+
+
+def get_same_products(hot_product):
+    return Product.objects.filter(category=hot_product.category).exclude(pk=hot_product.pk)[:3]
+
+
 def main(request):
     title = "главная"
     products = Product.objects.all()[:4]
-    content = {"title": title, "products": products, "media_url": settings.MEDIA_URL}
+    content = {
+        "title": title,
+        "products": products,
+        "media_url": settings.MEDIA_URL,
+        'basket': get_basket(request.user),
+    }
     return render(request, 'mainapp/index.html', content)
 
 
 def products(request, pk=None):
     title = "продукты"
     links_menu = ProductCategory.objects.all()
-
-    basket = []
-    if request.user.is_authenticated:
-        basket = Basket.objects.filter(user=request.user)
+    basket = get_basket(request.user)
 
     if pk is not None:
         if pk == 0:
@@ -39,13 +55,15 @@ def products(request, pk=None):
         }
         return render(request, "mainapp/products_list.html", content)
 
-    same_products = Product.objects.all()
+    hot_product = get_hot_product()
+    same_products = get_same_products(hot_product)
     content = {
         "title": title,
         "links_menu": links_menu,
         "same_products": same_products,
         "media_url": settings.MEDIA_URL,
         "basket": basket,
+        "hot_product": hot_product,
     }
     return render(request, "mainapp/products.html", content)
 
@@ -53,5 +71,22 @@ def products(request, pk=None):
 def contact(request):
     title = "о нас"
     locations = Location.objects.all()
-    content = {"title": title, "locations": locations, "visit_date": datetime.now()}
+    content = {
+        "title": title,
+        "locations": locations,
+        "visit_date": datetime.now(),
+        'basket': get_basket(request.user),
+    }
     return render(request, 'mainapp/contact.html', content)
+
+
+def product(request, pk):
+    title = 'продукты'
+    content = {
+        'title': title,
+        'links_menu': ProductCategory.objects.all(),
+        'product': get_object_or_404(Product, pk=pk),
+        'basket': get_basket(request.user),
+        "media_url": settings.MEDIA_URL,
+    }
+    return render(request, 'mainapp/product.html', content)
